1f0c6da063ffcbf7f1760825e5094a06f955dce4
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-11-17 09:34:42 -0500

    submit hw6-2

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author: MOG 11/15/21
+
+# Question 1
+list = [1,2,3,4,5,6,7]
+
+# Question 2
+print(len(list))
+
+# Question 3
+print(list[2])
+
+# Question 4
+print(5 not in list)
+
+# Question 5
+print(10 not in list)
+
+# Question 6
+name_list = ["Colin", "Michael", "Mohamed", "Ryan"]
+
+# Question 7
+print(name_list[1])
+
+# Question 8
+print(name_list * 3)
+
+# Question 9
+list.extend(name_list)
+
+# Question 10
+print(len(list))
